6208c80d99859eebe9176dc646c7c5f41a345157
Naz <nazzzarr@gmail.com>
2021-10-05 11:30:56 +0300

    decomp useEffect localStorage

--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -16,9 +16,7 @@ function Timer() {
 		console.log('start component');
 		const timesFromLS = JSON.parse(localStorage.getItem('times'));
 		console.log(timesFromLS);
-		if (timesFromLS) {
-			setResult([...result, ...timesFromLS]);
-		}
+		timesFromLS && setResult([...result, ...timesFromLS]);
 	}, []);
 
 	useEffect(() => {
@@ -57,30 +55,6 @@ function Timer() {
 			setTime(Date.now() - startMoment);
 		}, 0);
 	};
-
-	// const handleTimeCalibration = (time) => {
-	// 	let ms = time % 1000;
-	// 	let sec = Math.trunc((time / 1000) % 60);
-	// 	let min = Math.trunc((time / 1000 / 60) % 60);
-	// 	let hr = Math.trunc(time / 1000 / 60 / 60);
-	// 	if (sec < 10) {
-	// 		sec = '0' + sec;
-	// 	}
-	// 	if (min < 10) {
-	// 		min = '0' + min;
-	// 	}
-	// 	if (hr < 10) {
-	// 		hr = '0' + hr;
-	// 	}
-	// 	const currentTimerObj = {
-	// 		hr,
-	// 		min,
-	// 		sec,
-	// 		ms,
-	// 	};
-
-	// 	return currentTimerObj;
-	// };
 
 	const formatedTime = handleTimeCalibration(time);
 
