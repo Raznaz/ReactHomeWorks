fe9c5b63915842085458eb052e5bb838702ff083
Naz <nazzzarr@gmail.com>
2021-10-24 21:27:15 +0300

    fix search input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,23 +1,19 @@
 import React, { useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { filterUsers } from '../../redux/actions/userActions';
 import Form from '../UI/Form/Form';
 import { Input } from '../UI/Input/Input';
 
 function Search() {
 	const dispatch = useDispatch();
-	const { filterValue } = useSelector((state) => state.usersArr);
 
 	const bodyInputRef = useRef();
-	console.log(bodyInputRef);
 
 	return (
 		<Form>
 			<Input
 				type="search"
 				placeholder="Search..."
-				// value={filterValue}
-				// onChange={(e) => dispatch(filterUsers(e.target.value))}
 				onChange={(e) =>
 					dispatch(filterUsers(bodyInputRef.current.value))
 				}
